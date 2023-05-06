a23ce12f027d165968dccbf679c3fc1f3778e113
Clastillo <max.grenier99@gmail.com>
2023-05-06 12:47:16 -0400

    Combinaison code photodiode

--- calcul.py
+++ calcul.py
@@ -20,7 +20,23 @@ temps_ini = time.perf_counter()
 # Photodiode
 #===============================================================
 
+r = 1070*10**(-4)
+surfacePhotoDiode = np.pi*r**2
+densiteSurfaciqueLum = 6.1
 
+Plumineuse = densiteSurfaciqueLum*surfacePhotoDiode
+
+responsivity = 0.55/1000 # amp per watt
+photocurrent = responsivity*Plumineuse
+
+jCap = 8*10**-12
+darkCurrent = 5*10**-9
+resistance = 1*10**9
+
+print("\nPhotodiode:")
+print("\tSurface photosensible de la photodiode :" + str(surfacePhotoDiode) + " cm\u00B2")
+print("\tPhotocurrent = "+ str(photocurrent) + " A")
+print("\tPuissance lumineuse = "+ str(Plumineuse) + " mW")
 
 #===============================================================
 # Etage 1
@@ -41,7 +57,7 @@ Rpi1 = Vt1/Ib1
 R01 = Va1/Ic1
 Gm1 = B1/Rpi1
 
-print("Etage 1:")
+print("\n\nEtage 1:")
 print("\tCourant Ie a l'equilibre: "+str(Ie1)+" A")
 print("\tCourant Ib a l'equilibre: "+str(Ib1)+" A")
 print("\tCourant Ic a l'equilibre: "+str(Ic1)+" A")
