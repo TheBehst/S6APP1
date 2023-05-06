6ef509dd03b4fcba964ee0667f6703246334122d
Clastillo <max.grenier99@gmail.com>
2023-05-06 12:37:52 -0400

    Calcul etage 1&2

---
+++ calcul.py
@@ -0,0 +1,108 @@
+import numpy as np
+import time
+
+Vmm = -10
+Vpp = 10
+
+R1 = 50000
+R2 = 80000
+R3 = 10000000
+R4 = 120000
+R5 = 1
+R6 = 50
+R7 = 220000
+R8 = 15000
+R10 = 0.01
+R11 = 200
+
+temps_ini = time.perf_counter()
+#===============================================================
+# Photodiode
+#===============================================================
+
+
+
+#===============================================================
+# Etage 1
+#===============================================================
+Vbe1 = 0.7
+Vt1 = 0.0259
+Va1 = 100
+B1 = 200
+
+Ie1 = (-Vbe1-Vmm)/R3
+Ib1 = Ie1/(B1+1)
+Ic1 = Ie1-Ib1
+
+Vce1 = Vpp-Vmm-R1*Ic1-R3*Ie1
+Vo1 = Vpp - R1*Ic1
+
+Rpi1 = Vt1/Ib1
+R01 = Va1/Ic1
+Gm1 = B1/Rpi1
+
+print("Etage 1:")
+print("\tCourant Ie a l'equilibre: "+str(Ie1)+" A")
+print("\tCourant Ib a l'equilibre: "+str(Ib1)+" A")
+print("\tCourant Ic a l'equilibre: "+str(Ic1)+" A")
+
+print("\n\tVoltage Vce a l'equilibre: "+str(Vce1)+" V")
+print("\tVoltage Vo1 a l'equilibre: "+str(Vo1)+" V")
+
+print("\n\tResistance Rpi a l'equilibre: " + str(Rpi1) + " \u03A9")
+print("\tResistance R0 a l'equilibre: " + str(R01) + " \u03A9")
+print("\tGain Gm: " + str(Gm1) + " V/A")
+
+#===============================================================
+# Etage 2
+#===============================================================
+k2 = 0.00303
+Vt2 = 0.7
+Va2 = 130
+
+Vg2 = (Vpp-Vmm)*R4/(R4+R2) + Vmm
+
+coeff2 = [ k2/2 , -1/R5 - k2*Vt2 , (Vg2-Vpp)/R5 + (k2*Vt2**2)/2]
+
+Vgs2 = -1*np.roots(coeff2)
+
+Vov2 = Vgs2[1]-Vt2
+
+Id2 = k2/2 * (Vgs2[1]-Vt2)**2
+Vo2 = Id2*R6+Vmm
+Vds2 = Vpp - Id2*(R5+R6) - Vmm
+
+ro2 = Va2/Id2
+
+gm2 = 2*Id2/Vov2
+
+print("\n\nEtage 2:")
+print("\tVoltage Vg: "+ str(Vg2) + " V")
+print("\tVoltage Vgs: " + str(Vgs2[1]) + " V")
+print("\t\t Calculer avec coefficient: " + str(coeff2))
+print("\tCourant Id: " + str(Id2) + " A")
+print("\tVoltage Vds: " + str(Vds2) + " V")
+print("\tVoltage Vov: " + str(Vov2) + " V")
+print("\tVoltage Vo2: " + str(Vo2) + " V")
+
+print("\n\tResistance Ro: " + str(ro2) + " \u03A9")
+print("\tGain Gm: " + str(gm2) + " A/V")
+
+#===============================================================
+# Etage 3
+#===============================================================
+
+Vi3 = (Vpp-Vmm)*R8/(R8+R7) + Vmm
+
+print("\n\nEtage 3:")
+print("\tVoltage Vi3: "+ str(Vi3) + " V")
+
+
+#===============================================================
+# Misc
+#===============================================================
+
+print("\n\n\nTemps de calcul: " + str(1000*(time.perf_counter()-temps_ini)) + " ms")
+
+
+
